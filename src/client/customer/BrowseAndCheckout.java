ac7a35012ef1ae064cc3fc357a7104d0907e8489
Brady Dean <2bdkid@gmail.com>
2020-11-17 14:11:27 -0500

    Updated BrowseAndCheckout program

--- src/client/customer/BrowseAndCheckout.java
+++ src/client/customer/BrowseAndCheckout.java
@@ -1,13 +1,13 @@
 package client.customer;
 
-import common.AdminController;
-import common.AdminDispatcher;
 import common.CustomerController;
 import common.CustomerDispatcher;
 import database.Item;
+import database.ItemUnavailable;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -61,19 +61,50 @@ public class BrowseAndCheckout {
             return;
         }
 
+        System.out.println();
         System.out.println("Available Items");
 
         for (int i = 0; i < items.size(); ++i) {
             Item item = items.get(i);
             System.out.printf("ID: %d%n", i);
             System.out.printf("Name: %s%n", item.getName());
-            System.out.printf("Price: $%f%n", item.getPrice());
+            System.out.printf("Price: $%.2f%n", item.getPrice());
             System.out.printf("Type: %s%n", item.getType());
             System.out.printf("Description: %s%n", item.getDescription());
             System.out.printf("Available quantity: %s%n", item.getQuantity());
             System.out.println();
         }
 
+        System.out.println("Add items to your cart by specifying the ID followed by the quantity requested");
+        System.out.println("Eg: 0 2");
+        System.out.println("Enter \"checkout\" when you are ready to checkout");
 
+        stdin.useDelimiter("\n");
+        String line = stdin.nextLine();
+
+        List<Item> cart = new ArrayList<>();
+
+        while (!line.equals("checkout")) {
+            line = stdin.nextLine();
+            Scanner linescanner = new Scanner(line);
+            int id = linescanner.nextInt();
+            int quantity = linescanner.nextInt();
+            Item item = items.get(id);
+            item.setQuantity(quantity);
+            cart.add(item);
+        }
+
+        try {
+            dispatcher.checkout(cart);
+        } catch (ItemUnavailable e) {
+            System.err.printf("Requested too much of item %s%n", e.getMessage());
+            System.out.println("Cart was not checked out");
+            return;
+        } catch (Exception e) {
+            System.err.printf("Exception: %s%n", e.getMessage());
+            return;
+        }
+
+        System.out.println("Cart was successfully checked out");
     }
 }
